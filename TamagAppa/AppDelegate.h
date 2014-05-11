027827f7b7d48960f830c4c97538099df6e1f33a
Lab User <labuser@wcc138-06r.its.carleton.edu>
2014-05-11 11:31:12 -0500

    Initial Commit

---
+++ TamagAppa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TamagAppa
+//
+//  Created by Lab User on 5/11/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TamagAppaTests/TamagAppaTests.m
@@ -0,0 +1,34 @@
+//
+//  TamagAppaTests.m
+//  TamagAppaTests
+//
+//  Created by Lab User on 5/11/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TamagAppaTests : XCTestCase
+
+@end
+
+@implementation TamagAppaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
