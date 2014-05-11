a0705ec21b304ae1d86c9756e959b536f96cad6d
everettm <labuser@wcc138-06r.its.carleton.edu>
2014-05-11 13:01:54 -0500

    added image files to project folder

--- TamagAppa/theBestChooseGameViewController.m
+++ TamagAppa/theBestChooseGameViewController.m
@@ -12,6 +12,7 @@
 @interface theBestChooseGameViewController ()
 @property NSArray *buttonList;
 @property NSMutableArray *randInts;
+@property int numAppas;
 
 @end
 
@@ -28,8 +29,9 @@
 - (void)appasAppear
 {
     [self appasDisappear];
+    self.view.userInteractionEnabled = NO;
     [_randInts removeAllObjects];
-    for (int i = 1; i<= 4; i++)
+    for (int i = 1; i<= _numAppas; i++)
     {
         int randInt = arc4random() %12;
         while([_randInts containsObject:[NSNumber numberWithInt: randInt]])
@@ -38,6 +40,11 @@
         }
         [_randInts addObject: [NSNumber numberWithInt: randInt]];
         [_buttonList[randInt] setImage:[UIImage imageNamed:@"Appa.png"]  forState:UIControlStateNormal];
+        [_buttonList[randInt] setImage:[UIImage imageNamed:@"Appa.png"]  forState:UIControlStateDisabled];
+    }
+    if (_numAppas <= 6)
+    {
+        _numAppas += 1;
     }
     [self performSelector:@selector(appasDisappear) withObject:nil afterDelay:2.0f];
 }
@@ -49,38 +56,16 @@
         [_buttonList[i] setImage:nil forState:UIControlStateNormal];
     }
     _gameInfo.text = @"";
-//    _button1.enabled = YES;
-//    _button2.enabled = YES;
-//    _button3.enabled = YES;
-//    _button4.enabled = YES;
-//    _button5.enabled = YES;
-//    _button6.enabled = YES;
-//    _button7.enabled = YES;
-//    _button8.enabled = YES;
-//    _button9.enabled = YES;
-//    _button10.enabled = YES;
-//    _button11.enabled = YES;
-//    _button12.enabled = YES;
+    self.view.userInteractionEnabled = YES;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _numAppas = 3;
     _gameInfo.text = @"";
     _randInts = [[NSMutableArray alloc] init];
     _buttonList = [[NSArray alloc] initWithObjects: _button1, _button2, _button3, _button4, _button5, _button6, _button7, _button8, _button9, _button10, _button11, _button12, nil];
-//    _button1.enabled = NO;
-//    _button2.enabled = NO;
-//    _button3.enabled = NO;
-//    _button4.enabled = NO;
-//    _button5.enabled = NO;
-//    _button6.enabled = NO;
-//    _button7.enabled = NO;
-//    _button8.enabled = NO;
-//    _button9.enabled = NO;
-//    _button10.enabled = NO;
-//    _button11.enabled = NO;
-//    _button12.enabled = NO;
     [self appasAppear];
 }
 
@@ -94,11 +79,11 @@
     {
         _gameInfo.text = @"Sorry, that was wrong.";
         [self performSelector:@selector(appasAppear) withObject:nil afterDelay:2.0f];
+        _numAppas -= 1;
     }
     else
     {
         [_randInts removeObject: [NSNumber numberWithInt: button - 1]];
-        //[_randInts removeObjectAtIndex: [_randInts indexOfObject: [NSNumber numberWithInt: button - 1]]
     }
     if ([_randInts count] == 0)
     {
