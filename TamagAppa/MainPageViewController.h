af38087e17696cd63be2f0b3e79d4196d0d46312
everettm <labuser@wcc138-16r.its.carleton.edu>
2014-05-22 18:09:05 -0500

    wedge appears when you click the watermelon
    next step: drag and drop

---
+++ TamagAppa/MainPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainPageViewController.h
+//  TamagAppa
+//
+//  Created by Lab User on 5/22/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainPageViewController : UIViewController
+
+@end
---
+++ TamagAppa/MainPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainPageViewController.m
+//  TamagAppa
+//
+//  Created by Lab User on 5/22/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import "MainPageViewController.h"
+
+@interface MainPageViewController ()
+
+@end
+
+@implementation MainPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
