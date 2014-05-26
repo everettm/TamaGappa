bca4626d17f13a49448dfef8aa19e72d1446292e
Lab User <labuser@wcc138-03r.its.carleton.edu>
2014-05-25 22:19:34 -0500

    placed elimination games in correct folder

---
+++ TamagAppa/EliminationGameViewController.h
@@ -0,0 +1,33 @@
+//
+//  EliminationGameViewController.h
+//  TamagAppa
+//
+//  Created by Lab User on 5/23/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EliminationGameViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+@property (weak, nonatomic) IBOutlet UIButton *button10;
+@property (weak, nonatomic) IBOutlet UIButton *button11;
+@property (weak, nonatomic) IBOutlet UIButton *button12;
+@property (weak, nonatomic) IBOutlet UIButton *button13;
+@property (weak, nonatomic) IBOutlet UIButton *button14;
+@property (weak, nonatomic) IBOutlet UIButton *button15;
+@property (weak, nonatomic) IBOutlet UIButton *resetGameButton;
+@property (weak, nonatomic) IBOutlet UILabel *gameMessage;
+@property (weak, nonatomic) IBOutlet UILabel *timeLeftLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLeftCountdown;
+
+
+@end
---
+++ TamagAppa/EliminationGameViewController.m
@@ -0,0 +1,231 @@
+//
+//  EliminationGameViewController.m
+//  TamagAppa
+//
+//  Created by Lab User on 5/23/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import "EliminationGameViewController.h"
+
+@interface EliminationGameViewController ()
+@property NSArray *buttonList;
+@property NSMutableArray *clickedButtonList;
+@property NSMutableArray *openSpaces;
+@property NSMutableArray *squareEliminationArray;
+@property NSNumber *trueObject;
+@property NSNumber *falseObject;
+@property NSDictionary *squareEliminationDictionary;
+@property int clickedButton1;
+@property int clickedButton2;
+@property NSTimer *countdownTimer;
+@property int secondsCount;
+
+@end
+
+@implementation EliminationGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)setTimer
+{
+    _secondsCount = 300;
+    _countdownTimer = [NSTimer scheduledTimerWithTimeInterval: 1.0 target:self selector:@selector(startTimer) userInfo: NULL repeats: YES];
+}
+
+- (void)startTimer
+{
+    _secondsCount -= 1;
+    int minutes = _secondsCount/60;
+    int seconds = _secondsCount - (minutes * 60);
+    NSString *timerOutput = [NSString stringWithFormat:@"%2d:%.2d", minutes, seconds];
+    _timeLeftCountdown.text = timerOutput;
+    if (_secondsCount == 0)
+    {
+        [_countdownTimer invalidate];
+        _countdownTimer = NULL;
+    }
+}
+
+- (void)startGame
+{
+    _gameMessage.text = @"";
+    [_clickedButtonList removeAllObjects];
+    [self setTimer];
+    int randInt = arc4random() % 15;
+    for (int i = 0; i < 15; i++)
+    {
+        if (i != randInt)
+        {
+            [_buttonList[i] setImage:[UIImage imageNamed:@"Appa.png"]  forState:UIControlStateNormal];
+            [_buttonList[i] setImage:[UIImage imageNamed:@"Appa.png"]  forState:UIControlStateDisabled];
+            _openSpaces[i] = _falseObject;
+        }
+        else
+        {
+            [_buttonList[i] setImage:NULL  forState:UIControlStateNormal];
+            [_buttonList[i] setImage:NULL  forState:UIControlStateDisabled];
+            _openSpaces[i] = _trueObject;
+        }
+    }
+}
+
+- (void) makeMove
+{
+    int button1Click = _clickedButton1 + 1;
+    int button2Click = _clickedButton2 + 1;
+    if (button2Click < 10) { button1Click *= 10; }
+    else { button1Click *= 100; }
+    int dictKey = button1Click + button2Click;
+    NSString *dictKeyID = [NSString stringWithFormat:@"%i", dictKey];
+    int squareToBeEliminated = [[_squareEliminationDictionary objectForKey:dictKeyID] integerValue] - 1;
+    if (squareToBeEliminated == -1) { return; }
+    UIButton *currentButton = _buttonList[squareToBeEliminated];
+    [currentButton setImage:NULL forState:UIControlStateNormal];
+    [currentButton setImage:NULL forState:UIControlStateDisabled];
+    [_buttonList[_clickedButton1] setImage:NULL forState:UIControlStateNormal];
+    [_buttonList[_clickedButton1] setImage:NULL forState:UIControlStateDisabled];
+    [_buttonList[_clickedButton2] setImage:[UIImage imageNamed:@"Appa.png"] forState:UIControlStateNormal];
+    [_buttonList[_clickedButton2] setImage:[UIImage imageNamed:@"Appa.png"] forState:UIControlStateDisabled];
+    _openSpaces[_clickedButton1] = _trueObject;
+    _openSpaces[_clickedButton2] = _falseObject;
+    _openSpaces[squareToBeEliminated] = _trueObject;
+    [self checkForGameOver];
+}
+
+- (void) checkForGameOver
+{
+    int numOpenSpaces = 0;
+    BOOL validMovesLeft = FALSE;
+    for (int i = 0; i < [_openSpaces count]; i++)
+    {
+        if (_openSpaces[i] == _trueObject) { numOpenSpaces += 1; }
+    }
+    if (numOpenSpaces == 14) { _gameMessage.text = @"Congratulations, you win!"; }
+    for (int i = 0; i < 15; i++)
+    {
+        if (_openSpaces[i] == _falseObject)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                int button1Click = i + 1;
+                int button2Click = j + 1;
+                if (button2Click < 10) { button1Click *= 10; }
+                else { button1Click *= 100; }
+                int dictKey = button1Click + button2Click;
+                NSString *dictKeyID = [NSString stringWithFormat:@"%i", dictKey];
+                int squareToBeEliminated = [[_squareEliminationDictionary objectForKey:dictKeyID] integerValue] - 1;
+                //NSLog(@"%@", dictKeyID);
+                if (squareToBeEliminated != -1)
+                {
+                    NSLog(@"1");
+                    if (_openSpaces[i] == _falseObject && _openSpaces[j] == _trueObject)
+                    {
+                        NSLog(@"2");
+                        if (_openSpaces[squareToBeEliminated] == _falseObject)
+                        {
+                            NSLog(@"3");
+                            NSLog(@"%@", dictKeyID);
+                            NSLog(@"%i", squareToBeEliminated + 1);
+                            NSLog(@"%@", _openSpaces[i]);
+                            NSLog(@"%@", _openSpaces[j]);
+                            validMovesLeft = TRUE;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        if (validMovesLeft == TRUE)
+        {
+            return;
+        }
+    }
+    NSLog(@"\n");
+    if (validMovesLeft == FALSE)
+    {
+        _gameMessage.text = @"No more valid moves. Try again.";
+    }
+}
+
+- (void) checkValidity
+{
+    if ([_clickedButtonList count] == 2)
+    {
+        _clickedButton1 = [_clickedButtonList[0] intValue];
+        _clickedButton2 = [_clickedButtonList[1] intValue];
+        [_clickedButtonList removeAllObjects];
+        [self resetButtons];
+        if (_openSpaces[_clickedButton1] == _trueObject || _openSpaces[_clickedButton2] == _falseObject)
+        {
+            return;
+        }
+        else
+        {
+            [self makeMove];
+        }
+    }
+}
+
+- (void) resetButtons
+{
+    for (int i = 0; i < 15; i++)
+    {
+        UIButton *buttonToReset = _buttonList[i];
+        buttonToReset.selected = NO;
+        buttonToReset.layer.borderWidth = 0.0f;
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _squareEliminationDictionary = @{@"14": @"2", @"16": @"3", @"27": @"4", @"29": @"5", @"38": @"5", @"310": @"6", @"41": @"2", @"46": @"5", @"411": @"7", @"413": @"8", @"512": @"8", @"514": @"9", @"61": @"3", @"64": @"5", @"613": @"9", @"615": @"10", @"72": @"4", @"79": @"8", @"83": @"5", @"810": @"9", @"92": @"5", @"97": @"8", @"103": @"6", @"108": @"9", @"114": @"7", @"1113": @"12", @"125": @"8", @"1214": @"13", @"134": @"8", @"136": @"9", @"1311": @"12", @"1315": @"14", @"145": @"9", @"1412": @"13", @"156": @"10", @"1513": @"14"};
+    _buttonList = [[NSArray alloc] initWithObjects: _button1, _button2, _button3, _button4, _button5, _button6, _button7, _button8, _button9, _button10, _button11, _button12, _button13, _button14, _button15, nil];
+    _clickedButtonList = [[NSMutableArray alloc] init];
+    _trueObject = [NSNumber numberWithBool:YES];
+    _falseObject = [NSNumber numberWithBool:NO];
+    _openSpaces = [[NSMutableArray alloc] initWithObjects: _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, _trueObject, nil];
+    [self resetButtons];
+    [self startGame];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)buttonClick:(id)sender
+{
+    int senderID = [sender tag];
+    UIButton *clickedButton = _buttonList[senderID];
+    if (clickedButton.selected)
+    {
+        clickedButton.selected = NO;
+        clickedButton.layer.borderWidth=0.0f;
+        [_clickedButtonList removeAllObjects];
+    }
+    else
+    {
+        clickedButton.selected = YES;
+        clickedButton.layer.borderWidth = 1.0f;
+        clickedButton.layer.borderColor = [[UIColor greenColor] CGColor];
+        [_clickedButtonList addObject: [NSNumber numberWithInt: [sender tag]]];
+        if ([_clickedButtonList count] == 2)
+        [self checkValidity];
+    }
+}
+
+- (IBAction)resetGameButtonClick:(id)sender {
+    [self resetButtons];
+    [self startGame];
+}
+
+@end
