92a53370e2868d69aa9acea1e9c8344c34ef0b4d
Lab User <labuser@wcc138-09r.its.carleton.edu>
2014-06-01 15:44:47 -0500

    Adding some appacopter files

---
+++ TamagAppa/AppaCopterViewController.h
@@ -0,0 +1,15 @@
+//
+//  AppaCopterViewController.h
+//  TamagAppa
+//
+//  Created by Lab User on 5/28/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppaCopterViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *flyingAppa;
+@property (strong, nonatomic) IBOutlet UILongPressGestureRecognizer *theMagicButton;
+
+@end
---
+++ TamagAppa/FlyingAppa.h
@@ -0,0 +1,13 @@
+//
+//  FlyingAppa.h
+//  TamagAppa
+//
+//  Created by Lab User on 6/1/14.
+//  Copyright (c) 2014 Team G. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlyingAppa : NSObject
+
+@end
