799a2d2bea62d621909845477da97c4dc8baf7fa
gaw0305 <labuser@wcc138-03r.its.carleton.edu>
2014-06-03 22:04:19 -0500

    pushpushpush

--- TamagAppa/EliminationGameViewController.h
+++ TamagAppa/EliminationGameViewController.h
@@ -17,6 +17,32 @@
 @property (weak, nonatomic) IBOutlet UITextView *startMessage;
 @property (weak, nonatomic) IBOutlet UIButton *revertALevelButton;
 @property (weak, nonatomic) IBOutlet UITextView *startGameTextField;
+@property NSMutableArray *clickedButtonList;
+@property NSMutableArray *openSpaces;
+@property NSNumber *trueObject;
+@property NSNumber *falseObject;
+@property NSDictionary *squareEliminationDictionary;
+@property (nonatomic, assign) int clickedButton1;
+@property (nonatomic, assign) int clickedButton2;
+@property (nonatomic, assign) int curLevel;
+@property (nonatomic, assign) BOOL gameWon;
+@property (nonatomic, assign) int numTriesLeft;
+@property (weak, nonatomic) IBOutlet UILabel *levelLabel;
 
+- (void) startGame;
+- (void) makeMove;
+- (void) checkForGameOver;
+- (void) checkValidity;
+- (BOOL) checkValidityWithDictionary:(int)button1Click :(int)button2Click;
+- (void) resetButtons:(BOOL)reset;
+- (void) viewDidLoad;
+- (IBAction) startButtonClick:(id)sender;
+- (IBAction) buttonClick:(id)sender;
+- (IBAction) resetGameButtonClick:(id)sender;
+- (void) resetUserDefaults;
+- (void) viewWillDisappear:(BOOL)animated;
+- (IBAction)goBackLevelButtonClick:(id)sender;
+- (BOOL) textFieldShouldBeginEditing:(UITextField*)textField;
+- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex;
 
 @end
--- TamagAppa/EliminationGameViewController.m
+++ TamagAppa/EliminationGameViewController.m
@@ -15,55 +15,57 @@
 #import "EliminationGameViewController.h"
 
 @interface EliminationGameViewController ()
-@property NSMutableArray *clickedButtonList;
-@property NSMutableArray *openSpaces;
-@property NSMutableArray *squareEliminationArray;
-@property NSNumber *trueObject;
-@property NSNumber *falseObject;
-@property NSDictionary *squareEliminationDictionary;
-@property int clickedButton1;
-@property int clickedButton2;
-@property int curLevel;
-@property BOOL gameWon;
-@property int numTriesLeft;
 
 @end
 
 @implementation EliminationGameViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    NSUserDefaults* eliminationSettings;
 }
 
+@synthesize resetGameButton;
+@synthesize gameMessage;
+@synthesize numTriesLeftLabel;
+@synthesize startButton;
+@synthesize numTriesLeftStartLabel;
+@synthesize startMessage;
+@synthesize revertALevelButton;
+@synthesize startGameTextField;
+@synthesize clickedButtonList;
+@synthesize openSpaces;
+@synthesize trueObject;
+@synthesize falseObject;
+@synthesize squareEliminationDictionary;
+@synthesize clickedButton1;
+@synthesize clickedButton2;
+@synthesize curLevel;
+@synthesize gameWon;
+@synthesize numTriesLeft;
+@synthesize levelLabel;
+
+
 // Starts the game. Loads the previous spaces for the apps, if necessary. Otherwise, starts the blank space in a random space.
-- (void)startGame
+- (void) startGame
 {
-    _gameMessage.text = @"";
-
-    _numTriesLeftLabel.text = [NSString stringWithFormat:@"%i",_numTriesLeft];
-
-    [_clickedButtonList removeAllObjects];
-    if ([[NSUserDefaults standardUserDefaults] integerForKey:@"eliminationLevel"] == 1)
+    gameMessage.text = @"";
+    numTriesLeftLabel.text = [NSString stringWithFormat:@"%i",numTriesLeft];
+    [clickedButtonList removeAllObjects];
+    if ([eliminationSettings integerForKey:@"eliminationLevel"] == 1)
     {
-        [_revertALevelButton setEnabled:NO];
-        [_revertALevelButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [revertALevelButton setEnabled:NO];
+        [revertALevelButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
     }
     else
     {
-        [_revertALevelButton setEnabled:YES];
-        [_revertALevelButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [revertALevelButton setEnabled:YES];
+        [revertALevelButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     }
-    BOOL prevData = [[NSUserDefaults standardUserDefaults] boolForKey:@"eliminationLoadPrevData"];
+    BOOL prevData = [eliminationSettings boolForKey:@"eliminationLoadPrevData"];
     if (prevData)
     {
         for (int i = 1; i <= 15; i++)
         {
-            if (_openSpaces[i-1] == _falseObject)
+            if (openSpaces[i-1] == falseObject)
             {
                 [(UIButton*)[self.view viewWithTag:i] setImage:[UIImage imageNamed:@"Appa.png"]  forState:UIControlStateNormal];
             }
@@ -81,12 +83,12 @@
             if (i != randInt)
             {
                 [(UIButton*)[self.view viewWithTag:i] setImage:[UIImage imageNamed:@"Appa.png"]  forState:UIControlStateNormal];
-                _openSpaces[i-1] = _falseObject;
+                openSpaces[i-1] = falseObject;
             }
             else
             {
                 [(UIButton*)[self.view viewWithTag:i] setImage:NULL  forState:UIControlStateNormal];
-                _openSpaces[i-1] = _trueObject;
+                openSpaces[i-1] = trueObject;
             }
         }
         
@@ -96,18 +98,18 @@
 // If a move is determined to be valid, updates the game board to reflect that move
 - (void) makeMove
 {
-    int button1Click = _clickedButton1;
-    int button2Click = _clickedButton2;
+    int button1Click = clickedButton1;
+    int button2Click = clickedButton2;
     if (button2Click < 10) { button1Click *= 10; }
     else { button1Click *= 100; }
-    int squareToBeEliminated = [[_squareEliminationDictionary objectForKey:[NSString stringWithFormat:@"%i", button1Click + button2Click]] integerValue];
+    int squareToBeEliminated = [[squareEliminationDictionary objectForKey:[NSString stringWithFormat:@"%i", button1Click + button2Click]] integerValue];
     if (squareToBeEliminated == -1) { return; }
     [(UIButton*)[self.view viewWithTag:squareToBeEliminated] setImage:NULL forState:UIControlStateNormal];
-    [(UIButton*)[self.view viewWithTag:_clickedButton1] setImage:NULL forState:UIControlStateNormal];
-    [(UIButton*)[self.view viewWithTag:_clickedButton2] setImage:[UIImage imageNamed:@"Appa.png"] forState:UIControlStateNormal];
-    _openSpaces[_clickedButton1-1] = _trueObject;
-    _openSpaces[_clickedButton2-1] = _falseObject;
-    _openSpaces[squareToBeEliminated-1] = _trueObject;
+    [(UIButton*)[self.view viewWithTag:clickedButton1] setImage:NULL forState:UIControlStateNormal];
+    [(UIButton*)[self.view viewWithTag:clickedButton2] setImage:[UIImage imageNamed:@"Appa.png"] forState:UIControlStateNormal];
+    openSpaces[clickedButton1-1] = trueObject;
+    openSpaces[clickedButton2-1] = falseObject;
+    openSpaces[squareToBeEliminated-1] = trueObject;
     [self checkForGameOver];
 }
 
@@ -116,26 +118,26 @@
 {
     int numOpenSpaces = 0;
     BOOL validMovesLeft;
-    for (int i = 0; i < [_openSpaces count]; i++)
+    for (int i = 0; i < [openSpaces count]; i++)
     {
-        if (_openSpaces[i] == _trueObject) { numOpenSpaces += 1; }
+        if (openSpaces[i] == trueObject) { numOpenSpaces += 1; }
     }
     if (numOpenSpaces == 14)
     {
-        _gameMessage.text = @"Level up!";
-        _gameWon = YES;
-        _curLevel += 1;
-        [[NSUserDefaults standardUserDefaults] setInteger:_curLevel forKey:@"eliminationLevel"];
-        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"eliminationLoadPrevData"];
-        [_resetGameButton setEnabled: NO];
+        gameWon = YES;
+        curLevel += 1;
+        [eliminationSettings setInteger:curLevel forKey:@"eliminationLevel"];
+        [eliminationSettings setBool:NO forKey:@"eliminationLoadPrevData"];
+        [resetGameButton setEnabled: NO];
         self.view.userInteractionEnabled = NO;
-        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Level up!" message:@"Do you want to keep playing?" delegate:self cancelButtonTitle:@"Yes" otherButtonTitles:@"No", nil];
+        [alert show];
     }
     else
     {
         for (int i = 1; i <= 15; i++)
         {
-            if (_openSpaces[i-1] == _falseObject)
+            if (openSpaces[i-1] == falseObject)
             {
                 for (int j = 1; j <= 15; j++)
                 {
@@ -144,28 +146,24 @@
                 }
             }
         }
-        _numTriesLeftLabel.text = [NSString stringWithFormat:@"%i", _numTriesLeft];
         if (!validMovesLeft)
         {
-            _gameMessage.text = @"No more valid moves.";
-        }
-        if (_numTriesLeft == 0)
-        {
-            _gameMessage.text = @"You lose.";
+            gameMessage.text = @"No more valid moves.";
+            if (numTriesLeft == 1) { gameMessage.text = @"You lose."; }
         }
     }
 }
 
 - (void) checkValidity
 {
-    if ([_clickedButtonList count] == 2)
+    if ([clickedButtonList count] == 2)
     {
-        _clickedButton1 = [_clickedButtonList[0] intValue];
-        _clickedButton2 = [_clickedButtonList[1] intValue];
-        BOOL validMove = [self checkValidityWithDictionary:_clickedButton1: _clickedButton2];
-        [_clickedButtonList removeAllObjects];
+        clickedButton1 = [clickedButtonList[0] intValue];
+        clickedButton2 = [clickedButtonList[1] intValue];
+        BOOL validMove = [self checkValidityWithDictionary:clickedButton1: clickedButton2];
+        [clickedButtonList removeAllObjects];
         [self resetButtons:NO];
-        if (_openSpaces[_clickedButton1-1] == _trueObject || _openSpaces[_clickedButton2-1] == _falseObject || !validMove)
+        if (openSpaces[clickedButton1-1] == trueObject || openSpaces[clickedButton2-1] == falseObject || !validMove)
         {
             return;
         }
@@ -178,20 +176,16 @@
 
 - (BOOL) checkValidityWithDictionary:(int)button1Click :(int)button2Click
 {
-    if (_openSpaces[button1Click - 1] == _falseObject && _openSpaces[button2Click - 1] == _trueObject)
+    if (openSpaces[button1Click - 1] == falseObject && openSpaces[button2Click - 1] == trueObject)
     {
         if (button2Click < 10) { button1Click *= 10; }
         else { button1Click *= 100; }
         int dictKey = button1Click + button2Click;
         NSString *dictKeyID = [NSString stringWithFormat:@"%i", dictKey];
-        int squareToBeEliminated = [[_squareEliminationDictionary objectForKey:dictKeyID] integerValue] - 1;
+        int squareToBeEliminated = [[squareEliminationDictionary objectForKey:dictKeyID] integerValue] - 1;
         if (squareToBeEliminated != -1)
         {
-            if (_openSpaces[squareToBeEliminated] == _falseObject)
-            {
-                
-                return YES;
-            }
+            if (openSpaces[squareToBeEliminated] == falseObject) { return YES; }
         }
     }
     return NO;
@@ -206,127 +200,145 @@
         buttonToReset.layer.borderWidth = 1.0f;
         buttonToReset.layer.borderColor = [[UIColor blackColor] CGColor];
     }
-    if (reset) { [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"eliminationLoadPrevData"]; }
-    [_resetGameButton setEnabled: YES];
+    if (reset) { [eliminationSettings setBool:NO forKey:@"eliminationLoadPrevData"]; }
 }
 
 - (void)viewDidLoad
 {
-    _clickedButtonList = [[NSMutableArray alloc] init];
-    _trueObject = [NSNumber numberWithBool:YES];
-    _falseObject = [NSNumber numberWithBool:NO];
-    _gameWon = NO;
     [super viewDidLoad];
+    [self.view bringSubviewToFront:startMessage];
+    [self.view bringSubviewToFront:numTriesLeftStartLabel];
+    [self.view bringSubviewToFront:startButton];
+    [self.view bringSubviewToFront:levelLabel];
+    self.view.userInteractionEnabled = YES;
+    eliminationSettings = [NSUserDefaults standardUserDefaults];
+    clickedButtonList = [[NSMutableArray alloc] init];
+    trueObject = [NSNumber numberWithBool:YES];
+    falseObject = [NSNumber numberWithBool:NO];
+    gameWon = NO;
     UIImageView *backgroundImage = [[UIImageView alloc] initWithFrame:self.view.frame];
     [backgroundImage setImage:[UIImage imageNamed:@"background"]];
     [self.view addSubview:backgroundImage];
     [self.view sendSubviewToBack:backgroundImage];
-    if ([[NSUserDefaults standardUserDefaults] integerForKey:@"eliminationLevel"] == 0)
+    if ([eliminationSettings integerForKey:@"eliminationLevel"] == 0)
     {
-        [[NSUserDefaults standardUserDefaults] setInteger:1 forKey:@"eliminationLevel"];
-        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"eliminationLoadPrevData"];
+        [eliminationSettings setInteger:1 forKey:@"eliminationLevel"];
+        [eliminationSettings setBool:NO forKey:@"eliminationLoadPrevData"];
+        [eliminationSettings synchronize];
     }
-    _curLevel = [[NSUserDefaults standardUserDefaults] integerForKey:@"eliminationLevel"];
-    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"eliminationLoadPrevData"])
+    curLevel = [eliminationSettings integerForKey:@"eliminationLevel"];
+    if ([eliminationSettings boolForKey:@"eliminationLoadPrevData"])
     {
-
-        //[[NSUserDefaults standardUserDefaults] setInteger:10 forKey:@"numTriesLeft"];
-
-        _numTriesLeft = [[NSUserDefaults standardUserDefaults] integerForKey:@"numTriesLeft"];
-        _openSpaces = [[[NSUserDefaults standardUserDefaults] objectForKey:@"currentConfiguration"] mutableCopy];
+        numTriesLeft = [eliminationSettings integerForKey:@"numTriesLeft"];
+        openSpaces = [[eliminationSettings objectForKey:@"currentConfiguration"] mutableCopy];
     }
     else
     {
-        _numTriesLeft = 11 - _curLevel;
-        _openSpaces = [[NSMutableArray alloc] init];
+        numTriesLeft = 11 - curLevel;
+        openSpaces = [[NSMutableArray alloc] init];
         for (int i = 0; i < 15; i++)
         {
-            [_openSpaces addObject: _trueObject];
+            [openSpaces addObject: trueObject];
         }
     }
-    _numTriesLeftStartLabel.text = [NSString stringWithFormat:@"%i", _numTriesLeft];
-    _squareEliminationDictionary = @{@"14": @"2", @"16": @"3", @"27": @"4", @"29": @"5", @"38": @"5", @"310": @"6", @"41": @"2", @"46": @"5", @"411": @"7", @"413": @"8", @"512": @"8", @"514": @"9", @"61": @"3", @"64": @"5", @"613": @"9", @"615": @"10", @"72": @"4", @"79": @"8", @"83": @"5", @"810": @"9", @"92": @"5", @"97": @"8", @"103": @"6", @"108": @"9", @"114": @"7", @"1113": @"12", @"125": @"8", @"1214": @"13", @"134": @"8", @"136": @"9", @"1311": @"12", @"1315": @"14", @"145": @"9", @"1412": @"13", @"156": @"10", @"1513": @"14"};
-    _startGameTextField.editable = NO;
+    if (numTriesLeft == 0) { numTriesLeft = 1; }
+    numTriesLeftStartLabel.text = [NSString stringWithFormat:@"%i", numTriesLeft];
+    levelLabel.text = [NSString stringWithFormat:@"%i", curLevel];
+    squareEliminationDictionary = @{@"14": @"2", @"16": @"3", @"27": @"4", @"29": @"5", @"38": @"5", @"310": @"6", @"41": @"2", @"46": @"5", @"411": @"7", @"413": @"8", @"512": @"8", @"514": @"9", @"61": @"3", @"64": @"5", @"613": @"9", @"615": @"10", @"72": @"4", @"79": @"8", @"83": @"5", @"810": @"9", @"92": @"5", @"97": @"8", @"103": @"6", @"108": @"9", @"114": @"7", @"1113": @"12", @"125": @"8", @"1214": @"13", @"134": @"8", @"136": @"9", @"1311": @"12", @"1315": @"14", @"145": @"9", @"1412": @"13", @"156": @"10", @"1513": @"14"};
+    startGameTextField.editable = NO;
+    //[self resetUserDefaults];
     [self resetButtons:NO];
     [self startGame];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)startButtonClick:(id)sender
 {
-    [super didReceiveMemoryWarning];
-}
-
-- (IBAction)startButtonClick:(id)sender {
     self.view.userInteractionEnabled = YES;
-    [self.view sendSubviewToBack:_startMessage];
-    [self.view sendSubviewToBack:_startButton];
-    _numTriesLeftStartLabel.text = @"";
+    [self.view sendSubviewToBack:startMessage];
+    [self.view sendSubviewToBack:startButton];
+    [self.view sendSubviewToBack:numTriesLeftStartLabel];
+    [self.view sendSubviewToBack:levelLabel];
 }
 
 - (IBAction)buttonClick:(id)sender
 {
     UIButton *clickedButton = (UIButton*)sender;
-    if (_openSpaces[clickedButton.tag-1] == _falseObject || [_clickedButtonList count] == 1)
+    if (openSpaces[clickedButton.tag-1] == falseObject || [clickedButtonList count] == 1)
     {
         if (clickedButton.selected)
         {
             clickedButton.selected = NO;
             clickedButton.layer.borderWidth=1.0f;
             clickedButton.layer.borderColor = [[UIColor blackColor] CGColor];
-            [_clickedButtonList removeAllObjects];
+            [clickedButtonList removeAllObjects];
         }
         else
         {
             clickedButton.selected = YES;
             clickedButton.layer.borderWidth = 1.0f;
             clickedButton.layer.borderColor = [[UIColor greenColor] CGColor];
-            [_clickedButtonList addObject: [NSNumber numberWithInt: [sender tag]]];
-            if ([_clickedButtonList count] == 2)
+            [clickedButtonList addObject: [NSNumber numberWithInt: [sender tag]]];
+            if ([clickedButtonList count] == 2)
                 [self checkValidity];
         }
     }
 }
 
-- (IBAction)resetGameButtonClick:(id)sender {
+- (IBAction)resetGameButtonClick:(id)sender
+{
+    numTriesLeft -= 1;
+    if (numTriesLeft == 1)
+    {
+        [resetGameButton setEnabled:NO];
+        [resetGameButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    }
+    numTriesLeftLabel.text = [NSString stringWithFormat:@"%i", numTriesLeft];
     [self resetButtons:YES];
     [self startGame];
 }
 
 - (void)resetUserDefaults
 {
-    [[NSUserDefaults standardUserDefaults] setObject:NULL forKey:@"currentConfiguration"];
-    [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"numTriesLeft"];
-    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"eliminationLoadPrevData"];
-    [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"eliminationLevel"];
+    [eliminationSettings setObject:NULL forKey:@"currentConfiguration"];
+    [eliminationSettings setInteger:0 forKey:@"numTriesLeft"];
+    [eliminationSettings setBool:NO forKey:@"eliminationLoadPrevData"];
+    [eliminationSettings setInteger:0 forKey:@"eliminationLevel"];
+    [eliminationSettings synchronize];
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
+- (void)viewWillDisappear:(BOOL)animated
+{
     [super viewWillDisappear:animated];
     if (self.isMovingFromParentViewController) {
-        if (!_gameWon)
+        if (!gameWon)
         {
-            [[NSUserDefaults standardUserDefaults] setObject:_openSpaces forKey:@"currentConfiguration"];
-
-            [[NSUserDefaults standardUserDefaults] setInteger:_numTriesLeft forKey:@"numTriesLeft"];
-
-            [[NSUserDefaults standardUserDefaults] setBool:!_gameWon forKey:@"eliminationLoadPrevData"];
+            [eliminationSettings setObject:openSpaces forKey:@"currentConfiguration"];
+            [eliminationSettings setInteger:numTriesLeft forKey:@"numTriesLeft"];
+            [eliminationSettings setBool:!gameWon forKey:@"eliminationLoadPrevData"];
         }
     }
 }
 
-- (IBAction)goBackLevelButtonClick:(id)sender {
-    _curLevel -= 1;
-    int curLevel = [[NSUserDefaults standardUserDefaults] integerForKey:@"eliminationLevel"];
-    [[NSUserDefaults standardUserDefaults] setInteger: curLevel-1 forKey:@"eliminationLevel"];
-    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"eliminationLoadPrevData"];
-    _numTriesLeft = 11 - _curLevel;
+- (IBAction)goBackLevelButtonClick:(id)sender
+{
+    curLevel -= 1;
+    [eliminationSettings setInteger: curLevel-1 forKey:@"eliminationLevel"];
+    [eliminationSettings setBool:NO forKey:@"eliminationLoadPrevData"];
+    numTriesLeft = 11 - curLevel;
+    levelLabel.text = [NSString stringWithFormat:@"%i", curLevel];
     [self startGame];
 }
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
 {
-    //[_startGameTextField resignFirstResponder];
+    //[startGameTextField resignFirstResponder];
     return NO;
+}
+
+- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 0) { [self viewDidLoad]; }
+    else { [self.navigationController popViewControllerAnimated:YES]; }
 }
 
 
